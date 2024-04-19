0ab2508517a32233a394930b5f0b28660972e781
yyj <yajunyuan12@163.com>
2024-04-19 10:12:02 +0800

    add c#testDeeplearn

---
+++ testDeepLearning/testDeepLearning/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace testDeepLearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(564, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 111);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始检测";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "选择模型文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(311, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "cls",
+            "obj",
+            "seg",
+            "obb"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndex = 2;
+            this.comboBox1.SelectedValue = "seg";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(111, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 56);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "选择图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ testDeepLearning/testDeepLearning/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using OpenCvSharp;
+
+namespace testDeepLearning
+{
+    public partial class Form1 : Form
+    {
+        string modelfile;
+        string mode;
+        string imgfile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [StructLayoutAttribute(LayoutKind.Sequential)]
+        public struct RecResult
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 100)]
+            public string imgname;
+            public int reallabel;
+            public int id;
+            public double confidence;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public int[] box;
+            public double radian;
+            public IntPtr boxMask;
+        };
+        [DllImport("DeepLearning.dll")]
+        static extern IntPtr AiGPUInit(String modelpath, String modelmode);
+        [DllImport("DeepLearning.dll")]
+        static extern void AIGPUDetectImg(IntPtr h, IntPtr data, int width, int height, int stride, out IntPtr result, out int outlen);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getSelectedValue_Click();
+            string qwq = @"D:\yolov8\ultralytics\runs\segment\train9\weights\best.engine";
+            string qwq1 = "seg";
+            IntPtr a = AiGPUInit(modelfile, mode);
+            string qwq2 = @"D:\yolov8\ultralytics\data\segdata\images\val\2-36.bmp";
+            Bitmap img = new Bitmap(imgfile);
+            BitmapData imgData = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
+            int outLen = 0;
+            IntPtr outputdata = IntPtr.Zero;
+            int outmaskLen = 0;
+            IntPtr ptr;
+            AIGPUDetectImg(a, imgData.Scan0, imgData.Width, imgData.Height, imgData.Stride, out ptr, out outLen);
+            // 将指针转换为结构体数组
+            //for (int i = 0; i < n; i++)
+            //{
+            //    // 计算每个元素的偏移量
+            //    IntPtr offset = IntPtr.Add(ptr, i * Marshal.SizeOf(typeof(OutputSeg)));
+            //    // 从指针中读取结构体
+            //    segs[i] = Marshal.PtrToStructure<OutputSeg>(offset);
+            //}
+            RecResult[] ads = new RecResult[outLen];
+            for (int i = 0; i < outLen; ++i)
+            {
+                ads[i] = (RecResult)Marshal.PtrToStructure(ptr + i * Marshal.SizeOf(typeof(RecResult)), typeof(RecResult));
+            }
+            Mat image = new Mat(qwq2, ImreadModes.Color);
+            foreach (var seg in ads)
+            {
+                Console.WriteLine($"id: {seg.id}, confidence: {seg.confidence}, box: [{string.Join(", ", seg.box)}], radian: {seg.radian}");
+                //根据innerLen字段确定内部结构体数组的长度
+                byte[,] segMasks = new byte[seg.box[3], seg.box[2]];
+                //byte[] innerSegs = new byte[seg.box[2]* seg.box[3]];
+                //for (int j = 0; j < seg.box[2] * seg.box[3]; j++)
+                //{
+                //    innerSegs[j] = (byte)Marshal.PtrToStructure(seg.boxMask + j * Marshal.SizeOf(typeof(byte)), typeof(byte));
+                //}
+                for (int i = 0; i < seg.box[3]; i++)
+                {
+                    for (int j = 0; j < seg.box[2]; j++)
+                    {
+                        segMasks[i, j] = (byte)Marshal.PtrToStructure(seg.boxMask + (i * seg.box[2] + j) * Marshal.SizeOf(typeof(byte)), typeof(byte));
+                    }
+                }
+                Mat aa = new Mat(seg.box[3], seg.box[2], MatType.CV_8UC1, segMasks);
+                Mat MyMat = new Mat(seg.box[3], seg.box[2], MatType.CV_8UC3, new Scalar(0, 0, 255));
+                Mat ImageROI = new Mat(image, new Rect(seg.box[0], seg.box[1], seg.box[2], seg.box[3]));
+                MyMat.CopyTo(ImageROI, aa);
+            }
+            Cv2.ImShow("chuli", image);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 创建打开文件对话框
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // 设置对话框的属性
+            //openFileDialog1.InitialDirectory = "C:\\";  // 设置默认文件夹
+            openFileDialog1.Filter = "模型文件(onnx,engine)|*.onnx;*.engine";  // 设置文件类型过滤器
+
+            // 显示打开文件对话框
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            // 处理用户的文件选择
+            if (result == DialogResult.OK)
+            {
+                // 获取用户所选文件的路径
+                modelfile = openFileDialog1.FileName;
+                button2.Text = modelfile;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 创建打开文件对话框
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // 设置对话框的属性
+            //openFileDialog1.InitialDirectory = "C:\\";  // 设置默认文件夹
+            openFileDialog1.Filter = "图片文件(jpg,jpeg,bmp,gif,ico,pen,tif)|*.jpg;*.jpeg;*.bmp;*.gif;*.ico;*.png;*.tif;*.wmf";  // 设置文件类型过滤器
+
+            // 显示打开文件对话框
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            // 处理用户的文件选择
+            if (result == DialogResult.OK)
+            {
+                // 获取用户所选文件的路径
+                imgfile = openFileDialog1.FileName;
+                button3.Text = imgfile;
+            }
+        }
+
+        private void getSelectedValue_Click()
+        {
+            // 获取ComboBox中当前选定项的值
+            mode = comboBox1.SelectedItem.ToString();
+
+        }
+    }
+}
